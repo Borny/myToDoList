7dbc68b86e449e141251ae1ef26bc0653a900ac9
Tristan Deloris <borntowebb@gmail.com>
2017-07-26 08:32:42 +0200

    created data object

--- js/main.js
+++ js/main.js
@@ -1,38 +1,56 @@
+// Recovering the 
+var data = {
+	todo: [],
+	completed: []
+};
+
+// Button to add task 
 var add = document.getElementById('add');
 
+// Triggers event on button
 add.addEventListener('click',function(){
 	var input = document.getElementById('input_todo');
 	var inputValue = document.getElementById('input_todo').value;
 
 	if(inputValue){
-		addItemTodo(inputValue);
-		input.value = '';
-		input.focus();
+		addItemTodo(inputValue); // function to add item with the input value as title
+		input.value = ''; 	// empties the input
+		input.focus();    	// gets the focus on the button after clicking
+
+		// adds the input value to the todo array
+		data.todo.push(inputValue);
 	} 
 
 });
 
-
+// Remove item function by clicking on remove icon
 function removeItem(){
 	var item = this.parentNode.parentNode,
 		parent = item.parentNode;
 
+	// removes item node
 	parent.removeChild(item);
 }
 
+// Moves the item by clicking on the completed task
 function moveCompletedItem(){
 	var item = this.parentNode.parentNode,
 		parent = item.parentNode,
-		id = parent.id;
+		id = parent.id,
+		value = item.innerText;
 
-		var target = (id === 'todo')? document.getElementById('completed'): document.getElementById('todo');
+	data.todo.slice(data.todo.indexOf(value));
+
+		// if : watches if the parent item is the todo most or completed list
+		// so that the target variable switches value
+	var target = (id === 'todo')? document.getElementById('completed'): document.getElementById('todo');
 			
 	parent.removeChild(item);
 	target.appendChild(item);
 
 }
 
-
+// Adds item to the todo list
 function addItemTodo(text){
 
 	var list = document.getElementById('todo');
